6fe41025eab1e528a7ed1597cf35ba3b608a498e
Fabien DI Tore <fabien@ditore.ch>
2017-10-19 15:58:01 +0200

    fixed script

--- Scripts/octoprint.sh
+++ Scripts/octoprint.sh
@@ -4,6 +4,9 @@ export IMAGE_PATH=/home/octoprint/.octoprint
 for dir in $(ls /srv/octoprint); do
   sudo docker stop octoprint-$dir
   sudo docker rm octoprint-$dir
-  sudo docker run -d -p 500$COUNT:5000  --name octoprint-$dir --device=/dev/ttyUSB$COUNT:/dev/ttyUSB0 -v /srv/octoprint/$dir:$IMAGE_PATH -v /srv/hive/slicingProfiles:$IMAGE_PATH/slicingProfiles -v   /srv/hive/uploads:$IMAGE_PATH/uploads  test/octoprint
+  sudo docker run -d -p 500$COUNT:5000  \
+    --name octoprint-$dir --device=/dev/ttyUSB$COUNT:/dev/ttyUSB0 \
+    -v /srv/octoprint/$dir:$IMAGE_PATH   test/octoprint
+
   COUNT=$(($COUNT+1))
 done;
